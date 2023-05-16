6ec1e4a507a0c5da219d537a12662f6e08be3b4d
marwamarwa2001 <121679870+marwamarwa2001@users.noreply.github.com>
2023-05-16 19:05:46 +0100

    Add files via upload

---
+++ player/js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+  {
+    name: "happiness is a butterfly",
+    artist: "lana del rey",
+    img: "img1",
+    audio: "music1"
+  },
+  {
+    name: "bohemiaen rhapsody",
+    artist: "queen",
+    img: "img2",
+    audio: "music2"
+  },
+  {
+    name: "soap",
+    artist: "mel",
+    img: "img3",
+    audio: "music3"
+  },
+  {
+    name: "as it was",
+    artist: "harry styles",
+    img: "img4",
+    audio: "music4"
+  }
+]
